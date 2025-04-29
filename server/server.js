d7c622a9374a5d8ae02f1c05a3f1ce42966f3994
Leopoldo Rezende <leopoldorezende@Leopoldos-MacBook-Pro.local>
2025-04-28 22:44:18 -0300

    corrigido porta para 3000 no server.js

--- server/server.js
+++ server/server.js
@@ -169,7 +169,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Countries data loaded: ${Object.keys(countriesData).length} countries`);
