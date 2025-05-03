b3bb989c65c4cdbf3de9c4fc71509139f0fe7afe
leopoldorezende <leopoldorezende@gmail.com>
2025-05-02 23:17:46 -0300

    novo firebase-admin

--- server/shared/firebaseAdmin.js
+++ server/shared/firebaseAdmin.js
@@ -1,14 +1,23 @@
-const admin = require('firebase-admin')
-const path = require('path')
+const admin = require('firebase-admin');
+const fs = require('fs');
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
+const keyPath = isProd 
+  ? '/root/keys/politik404-41c47-firebase-adminsdk-fbsvc-2c5ed453f9.json'
+  : require('path').join(__dirname, '../keys/firebase-admin.json');
 
-const serviceAccount = isProd
-  ? require('/root/keys/politik404-41c47-firebase-adminsdk-fbsvc-2c5ed453f9.json')
-  : require(path.join(__dirname, '../keys/firebase-admin.json'))
+let serviceAccount;
+
+try {
+  const rawData = fs.readFileSync(keyPath, 'utf8');
+  serviceAccount = JSON.parse(rawData);
+} catch (err) {
+  console.error('[FIREBASE] Erro ao carregar chave do Firebase Admin:', err.message);
+  process.exit(1); // derruba o servidor com erro explícito
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
-})
+});
 
-module.exports = admin
+module.exports = admin;
