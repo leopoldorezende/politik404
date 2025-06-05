7df3d5cda1c49384e60fad66c65ee21f032fd335
leopoldorezende <leopoldorezende@gmail.com>
2025-06-05 11:50:31 -0300

    dist

--- client/src/ui/sideview/Sideview.jsx
+++ client/src/ui/sideview/Sideview.jsx
@@ -167,7 +167,7 @@ const Sideview = ({ onExitRoom, onClose, isActive }) => {
               </tr>
               <tr>
                 <td>Acordos Internos</td>
-                <td>3</td>
+                <td>6</td>
                 <td>3 pontos</td>
               </tr>
               <tr>
